688e621c70d032446628c3cf35b43b6419ec0d5b
Otávio Barbosa Ferreira <85796227+otavio-barbosa@users.noreply.github.com>
2021-06-14 19:04:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function onOFF(){
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
